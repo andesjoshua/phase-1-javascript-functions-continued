17d25df9e18107e3b2e44939e6e07d457f3ce8e5
Joshua Andes <andesjoshua@comcast.net>
2021-09-21 09:28:54 -0400

    commit tree 3fcc73b3556ccb6516dba39ce32dcb00912f18c0

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+const saturdayFun = (activity = 'roller-skate') => {
+        return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = (work = 'go to the office') => {
+    return `This Monday, I will ${work}.`
+}
+
+const wrapAdjective = (visualFlare = '*') => {
+    return function(emphatic = 'a hard worker') {
+        return `You are ${visualFlare}${emphatic}${visualFlare}!`
+    };
+
+}
+// function demoChain(name) {
+//     const part1 = "hi";
+//     return function () {
+//       const part2 = "there";
+//       return function () {
+//         console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+//       };
+//     };
+//   }
+  
+//   demoChain("Dr. Stephen Strange")()();
